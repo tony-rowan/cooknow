a1cb5844309ae3b61997ac063c9033cbafda821c
Tony Rowan <tony-rowan@cookpad.com>
2023-03-15 16:21:13 +0000

    Don't initialize open ai module if it's not enabeld

--- config/initializers/open_ai.rb
+++ config/initializers/open_ai.rb
@@ -1,3 +1,5 @@
-OpenAI.configure do |config|
-  config.access_token = ENV.fetch("OPENAI_ACCESS_TOKEN")
+if ENV.fetch("OPEN_AI_ENABLED", false)
+  OpenAI.configure do |config|
+    config.access_token = ENV.fetch("OPENAI_ACCESS_TOKEN")
+  end
 end
